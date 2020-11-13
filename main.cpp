9f1cb251409273162393710486b3fc8125ff447e
Axel DERLY <axel.derly@student.yncrea.fr>
2020-11-13 12:01:54 +0100

    V3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
-
-using namespace std;
+#include <cstdlib>
+#include <SFML/Graphics.hpp>
 
 int main() {
-	cout << "Bonjour";
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!"); //,sf::Style::Fullscreen en paramètre de window pour un fullscreen qui pique aux yeux
+    sf::CircleShape shape(100.f, 4);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
