167f8865a85845f28a387c0b1d48c928818e75a0
Axel DERLY <axel.derly@student.yncrea.fr>
2020-12-05 12:22:09 +0100

    2

--- main.cpp
+++ main.cpp
@@ -2,22 +2,8 @@
 #include <cstdlib>
 #include <SFML/Graphics.hpp>
 
+using namespace std;
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!"); //,sf::Style::Fullscreen en paramètre de window pour un fullscreen qui pique aux yeux
-    sf::CircleShape shape(100.f, 4);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	cout << "3" << endl;
 }
